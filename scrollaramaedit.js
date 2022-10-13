0ccf777dacf9365d89f1ee859682ea1c9fd50358
ewhite-whyy <115742469+ewhite-whyy@users.noreply.github.com>
2022-10-13 16:42:22 -0400

    Update scrollaramaedit.js

--- scrollaramaedit.js
+++ scrollaramaedit.js
@@ -27,12 +27,15 @@
   	el.style.left = "0";
   	el.style.width = "100%";
   	el.style.zIndex = "9999";
+  	el.style.borderTop = "0px solid black";
+  	el.style.borderBottom = "0px solid black";
 
   	const p = document.createElement("p");
   	p.style.position = "absolute";
   	p.style.left = "0";
   	p.style.height = "1px";
   	p.style.width = "100%";
+  	p.style.borderTop = "0px dashed black";
 
   	el.appendChild(p);
   	document.body.appendChild(el);
