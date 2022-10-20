33ae5534c6cfe39d033bd8d6bdad200de51da9ab
ewhite-whyy <115742469+ewhite-whyy@users.noreply.github.com>
2022-10-20 16:25:18 -0400

    Add files via upload

--- scrollaramaedit.js
+++ scrollaramaedit.js
@@ -19,28 +19,6 @@
     }
     return [];
   }
-// SETUP
-  function create(className) {
-  	const el = document.createElement("div");
-  	el.className = `scrollama__debug-step ${className}`;
-  	el.style.position = "fixed";
-  	el.style.left = "0";
-  	el.style.width = "100%";
-  	el.style.zIndex = "9999";
-  	el.style.borderTop = "0px solid black";
-  	el.style.borderBottom = "0px solid black";
-
-  	const p = document.createElement("p");
-  	p.style.position = "absolute";
-  	p.style.left = "0";
-  	p.style.height = "1px";
-  	p.style.width = "100%";
-  	p.style.borderTop = "0px dashed black";
-
-  	el.appendChild(p);
-  	document.body.appendChild(el);
-  	return el;
-  }
 
 
   // UPDATE
